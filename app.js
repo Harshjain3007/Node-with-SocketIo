63f38da208b5597b7af2ffc670f28aa04f9a1526
Harshjain3007 <hjain3007@gmail.com>
2023-12-06 22:51:39 +0530

    NAMESPACES done

--- app.js
+++ app.js
@@ -13,15 +13,13 @@ app.get('/',function(req,res){
        res.sendFile(fileName,options)
 })
 
-var users =0 
-
-
-io.on('connection',function(socket){
-    console.log('A user connected');
-       users++
+//var users =0 
+//io.on('connection',function(socket){
+   // console.log('A user connected');
+      // users++
        //io.sockets.emit('broadcast',{message:users + 'users connected!'})
-       socket.emit('newuserconnect',{message:'Hey welcome dear'})
-       socket.broadcast.emit('newuserconnect',{message:users+"users connected!"})
+    //    socket.emit('newuserconnect',{message:'Hey welcome dear'})
+    //    socket.broadcast.emit('newuserconnect',{message:users+"users connected!"})
     // setTimeout(function(){
     //      // socket.send('send message from server side by prereserved event')
     //      socket.emit('myCustomEvent',{description:'A custom message from server side'});
@@ -31,11 +29,28 @@ io.on('connection',function(socket){
     //     console.log(data);
     // })
 
-    socket.on('disconnect',function(){
-        console.log('A user disconnected');
-        users--
-        io.sockets.emit('broadcast',{message:users + 'users connected!'}) 
-    })
+    // io.emit('testEvent',"TesterEvent call")
+
+    // socket.on('disconnect',function(){
+    //     console.log('A user disconnected');
+    //     users--
+    //     io.sockets.emit('broadcast',{message:users + 'users connected!'}) 
+    // })
+    
+//})
+
+var cnsp = io.of('/custom-namespace');
+
+cnsp.on('connection',function(socket){
+    console.log('A user connected');
+    
+cnsp.emit('customEvent',"TesterEvent call")
+
+    // socket.on('disconnect',function(){
+    //     console.log('A user disconnected');
+       // users--
+    //     io.sockets.emit('broadcast',{message:users + 'users connected!'}) 
+    // })
     
 })
 
